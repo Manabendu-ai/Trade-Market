d7fb95310848159698c33b669598c423e11c912a
Manabendu-ai <rikugithub@gmail.com>
2026-04-09 20:38:34 +0530

    template setup

---
+++ market.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/about/<username>')
+def about_page(username):
+    return f'<h1> This is the about page of {username} </h1>'
+
+if __name__ == "__main__":
+    app.run(debug = True)
